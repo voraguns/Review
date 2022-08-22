0d6f157815ba5531085df74b8d107185e50b73e3
Voragun Supawong <voragun2543@gmail.com>
2022-08-22 16:39:13 +0700

    🐾setup project and add class Element

--- code/Start.java
+++ code/Start.java
@@ -1,5 +1,11 @@
 class Start {
     public static void main(String[] data) {
-        
+        int[] a = { 8, 5, 4, 2, 7, 1, 9, 3 };
     }
+}
+
+class Element {
+    String name;
+    int value;
+    Element next;
 }
