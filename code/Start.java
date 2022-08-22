39b112e330d966212509fcbed7c3d84d87f47723
Voragun Supawong <voragun2543@gmail.com>
2022-08-22 16:52:59 +0700

    ⚽ [linked list] add createList function
    createList() is use for create linked list

--- code/Start.java
+++ code/Start.java
@@ -2,6 +2,25 @@ class Start {
     public static void main(String[] data) {
         int[] a = { 8, 5, 4, 2, 7, 1, 9, 3 };
     }
+    
+    static Element createList(int ... data) {
+        Element head = null;
+        Element tail = null;
+        
+        for(int i = 0; i < data.length; i++){
+            if(tail == null) {
+                tail = new Element();
+                tail.value = data[i];
+                head = tail;
+            } else {
+                tail.next = new Element();
+                tail.next.value = data[i];
+                tail = tail.next;
+            }
+        }
+        
+        return head;
+    }
 }
 
 class Element {
